08bc14b0195e448ac65b63ce32c72ab05c1d6a73
Sulejman Sarajlija <3107367+Sulejman@users.noreply.github.com>
2024-05-16 21:12:16 +0200

    Adding test ERC20 tokens

---
+++ contracts/utils/ERC2OToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor() ERC20("Tether USD", "USDT") {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
+
+contract NTZC is ERC20 {
+    constructor() ERC20("Net Zero Coin", "NTZC") {
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+}
